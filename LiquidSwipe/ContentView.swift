547cd65d18445efbdfcacbc51d1221d228ca3bfd
jefferymason <jefferymason72@gmail.com>
2021-05-10 09:49:46 -0500

    Initial Commit

---
+++ LiquidSwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiquidSwipe
+//
+//  Created by Jeff Mason on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiquidSwipe/LiquidSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeApp.swift
+//  LiquidSwipe
+//
+//  Created by Jeff Mason on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
